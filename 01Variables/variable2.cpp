34abcc2bfc00aa5bf338e90aaf81e8160f3ce42f
ritika02052007 <ritika02052007@gmail.com>
2026-06-12 18:39:35 +0530

    learning about variable

---
+++ 01Variables/variable2.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"enter the integer value :";
+    cin>>a;
+    cout<<"your value is :"<<a<<endl;
+
+    float price;
+    cout<<"enter the float value :";
+    cin>>price;
+    cout<<"your value is :"<<price<<endl;
+
+    double b;
+    cout<<"enter the double value :";
+    cin>>b;
+    cout<<"your value is :"<<b <<endl;
+
+    string name;
+    cout<<"enter the string value :";
+    cin>>name;
+    cout<<"your value is :"<<name<<endl;
+
+    char ch;
+    cout<<"enter the character value is :";
+    cin>>ch;
+    cout<<"your value is :"<<ch<<endl;
+
+    bool istrue;
+    cout<<"enter the boolean value :";
+    cin>>istrue;
+    cout<<"your value is :"<<istrue<<endl;
+
+    return 0;
+
+}
---
+++ 01Variables/variables.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 4;
+    char ch = 'a';
+    float price = 120.45;
+    string name = "ritika";
+    double amount = 1234.678;
+    bool istrue = true;
+    bool isfalse = false;
+
+    cout<<"Integer value :"<<a<<endl;
+    cout<<"character value :"<<ch<<endl;
+    cout<<"float value :"<<price<<endl;
+    cout<<"string value :"<<name<<endl;
+    cout<<"double value :"<<amount<<endl;
+    cout<<"bool value :"<<istrue<<endl;
+    cout<<"bool value :"<<isfalse<<endl;
+
+    return 0;
+}
